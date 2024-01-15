f2b8950cc2b1857b3567601b27bed52062503e28
rami <rami_zezo1986@hotmail.com>
2024-01-15 11:56:06 +0100

    test ssh key

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <main id="game-container">
+      <h1>hi</h1>
       <ol id="players">
         <Player initialPlayerName={"Player-1"} symbol={"X"}/>
         <Player initialPlayerName={"Player-2"} symbol={"O"}/>
